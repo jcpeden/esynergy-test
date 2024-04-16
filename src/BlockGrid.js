0c75f916e7331364c0e47b0d117befc7986334a8
John Peden <john.peden@moneysupermarket.com>
2024-04-16 14:26:23 +0100

    John Peden: Tech test for esynergy

--- src/BlockGrid.js
+++ src/BlockGrid.js
@@ -7,40 +7,149 @@ class BlockGrid {
     this.grid = [];
 
     for (let x = 0; x < this.width; x++) {
-      const col = [];
+      const column = [];
       for (let y = 0; y < this.height; y++) {
-        col.push(new Block(x, y));
+        column.push(new Block(x, y));
       }
-
-      this.grid.push(col);
+      this.grid.push(column);
     }
   }
 
-  render(el = document.getElementById('gridEl')) {
+  /**
+   * Renders the block grid in the specified element or default element.
+   * Creates DOM elements for each block in the grid and appends them to the specified container.
+   * Each block element is assigned an ID based on its coordinates and styled according to its color.
+   * Click event listeners are added to each block element to handle block clicks.
+   * @param {HTMLElement} [element=document.getElementById('gridEl')] The container element to render the grid into.
+   */
+  render(element = document.getElementById('gridEl')) {
     for (let x = 0; x < this.width; x++) {
-      const id = 'col_' + x;
-      const colEl = document.createElement('div');
-      colEl.id = id;
-      colEl.className = 'col';
-      el.appendChild(colEl);
+      const columnId = 'col_' + x;
+      const columnElement = document.createElement('div');
+      columnElement.id = columnId;
+      columnElement.className = 'col';
+      element.appendChild(columnElement);
 
       for (let y = this.height - 1; y >= 0; y--) {
         const block = this.grid[x][y];
-        const id = `block_${x}x${y}`;
-        const blockEl = document.createElement('div');
+        const blockId = `block_${x}x${y}`;
+        const blockElement = document.createElement('div');
 
-        blockEl.id = id;
-        blockEl.className = 'block';
-        blockEl.style.background = block.colour;
-        blockEl.addEventListener('click', evt => this.blockClicked(evt, block));
-        colEl.appendChild(blockEl);
+        blockElement.id = blockId;
+        blockElement.className = 'block';
+        blockElement.style.background = block.colour;
+        blockElement.addEventListener('click', evt => this.blockClicked(evt, block));
+        columnElement.appendChild(blockElement);
       }
     }
   }
 
-  blockClicked(e, block) {
-    console.log(e, block);
+  /**
+   * Handles the click event on a block.
+   * If the block has a non-null color, it initiates the removal of connected blocks with the same color,
+   * shifts blocks downward, resets the grid, and re-renders the grid.
+   * @param {Event} event The click event object.
+   * @param {Object} block The block object representing the clicked block.
+   */
+  blockClicked(event, block) {
+    const { x, y, colour } = block;
+
+    if (colour !== null) {
+      const removedBlockArray = [];
+
+      this.removeConnectedBlocks(x, y, colour, removedBlockArray)
+        .shiftBlocksDownward(removedBlockArray)
+        .resetGrid()
+        .render();
+    }
+  }
+
+  directions = [
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+    [1, 0],
+  ];
+
+  removeConnectedBlocks = (x, y, colour, removedBlockArray) => {
+    const adjacentBlocks = [];
+    const parsedBlocks = new Set();
+
+    adjacentBlocks.push([x, y]);
+
+    // While there are block available in the array to be parsed
+    while (adjacentBlocks.length > 0) {
+
+      // Get the row and column the last block from the array
+      const [row, column] = adjacentBlocks.pop();
+
+      if (this.outsideBoundaries(row, column) || parsedBlocks.has(`${row},${column}`)) {
+        continue;
+      } else {
+        parsedBlocks.add(`${row},${column}`);
+      }
+
+      if (this.checkBlockColour(row, column, colour)) {
+
+        // Remove the block from the grid by setting its colour to null
+        this.removeBlock(row, column)
+
+        // Update the array of blocks that have been removed
+        removedBlockArray.push([row, column]);
+
+        // Add all adjacent blocks into the adjacentBlocks array to be parsed
+        this.directions.forEach(([xOffset, yOffset]) => {
+          adjacentBlocks.push([row + xOffset, column + yOffset]);
+        });
+      }
+    }
+
+    return this;
+  }
+
+  checkBlockColour = (row, column, colour) => {
+    return this.grid[row][column].colour === colour
+  }
+
+  removeBlock = (row, column) => {
+    this.grid[row][column].colour = null;
+  }
+
+  outsideBoundaries = (row, column) => {
+    return row < 0 || column < 0 || row >= this.width || column >= this.height
+  }
+
+  // Shift blocks downward, starting from the bottom iterate upwards
+  shiftBlocksDownward(removedBlockArray) {
+    removedBlockArray.forEach(([x, y]) => {
+      for (let yPos = this.height - 1; yPos >= 0; yPos--) {
+        let currentRow = yPos;
+
+        while (this.blockBelowIsEmpty(x, currentRow)) {
+          this.moveBlockDown(x, currentRow);
+          currentRow++;
+        }
+      }
+    });
+    return this;
+  }
+
+  // Set lower block colour to that of the above block, then set colour of the above block to null
+  blockBelowIsEmpty(x, y) {
+    return this.grid[x][y].colour === null && this.grid[x][y + 1];
+  }
+
+  // Set lower block colour to that of the above block, then set colour of the above block to null
+  moveBlockDown(x, y) {
+    this.grid[x][y].colour = this.grid[x][y + 1].colour;
+    this.grid[x][y + 1].colour = null;
+  }
+
+  resetGrid() {
+    document.getElementById('gridEl').innerHTML = '';
+    return this;
   }
 }
 
 export default BlockGrid;
+
--- src/BlockGrid.test.js
+++ src/BlockGrid.test.js
@@ -1,28 +1,145 @@
 import BlockGrid from './BlockGrid';
 import Block from './Block';
 
+const CONSTANTS = {
+  primaryBlockArray: [
+    [0, 4],
+    [0, 3],
+    [0, 2],
+    [0, 1],
+    [0, 0],
+  ],
+  secondaryBlockArray: [
+    [1, 4],
+    [1, 3],
+    [1, 2],
+    [1, 1],
+    [1, 0],
+  ],
+  tertiaryBlockArray: [
+    [0, 9],
+    [0, 8],
+    [0, 7],
+    [0, 6],
+    [0, 5],
+  ],
+};
+
+
+const createWrapper = () => {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'gridEl');
+  document.body.appendChild(div);
+};
+
+const clickBlock = (id) => {
+  document.getElementById(id).click();
+};
+
+const assertBlockColor = (blockGrid, blockArray, colour) => {
+  blockArray.forEach(
+    ([x, y]) => expect(blockGrid.grid[x][y].colour === colour)
+  )
+}
+
+const setBlockArrayColour = (blockGrid, blockArray, colour) => {
+  blockArray.forEach(
+    ([x, y]) => (blockGrid.grid[x][y].colour = colour)
+  );
+}
+
+// Create a new block grid (defaults to 10x10)
+const setup = (width = 10, height = 10) => {
+  createWrapper();
+
+  return new BlockGrid(width, height)
+}
+
 describe('BlockGrid', () => {
-  it('fills a multidimensional array of Blocks as its grid, according to the given width and height', () => {
-    const grid = new BlockGrid(10, 10).grid;
+  let blockGrid;
 
-    expect(grid.length).toBe(10);
+  describe('when using default height and width values', () => {
+    beforeEach(() => {
+      blockGrid = setup();
 
-    grid.forEach(column => {
-      expect(column.length).toBe(10);
+      blockGrid.render();
+    })
 
-      column.forEach(block => {
-        expect(block).toBeInstanceOf(Block);
+    it('should create a multidimensional array of Blocks', () => {
+      expect(blockGrid.grid.length).toBe(10);
+
+      blockGrid.grid.forEach(column => {
+        expect(column.length).toBe(10);
+
+        column.forEach(block => {
+          expect(block).toBeInstanceOf(Block);
+        });
+      });
+    })
+
+    describe('and when a user clicks on a block', () => {
+      beforeEach(() => {
+        clickBlock('block_0x9');
+      })
+
+      it('should "remove" that block by setting its background to null', () => {
+        expect(document.getElementById('block_0x9').style.background).toBe('');
+      })
+    })
+
+    describe('when adjacent blocks have the same colour', () => {
+      const { primaryBlockArray, secondaryBlockArray, tertiaryBlockArray } = CONSTANTS;
+
+      beforeEach(() => {
+        // Reassign primaryBlockArray (col 0, bottom) blockArray colour to blue
+        setBlockArrayColour(blockGrid, primaryBlockArray, 'red')
+
+        // Reassign secondaryBlockArray (col 1, bottom) blockArray colour to blue
+        setBlockArrayColour(blockGrid, secondaryBlockArray, 'blue')
+
+        // Reassign tertiaryBlockArray (col 0, top) blockArray colour to green
+        setBlockArrayColour(blockGrid, tertiaryBlockArray, 'green')
+
+        // Render the grid
+        blockGrid.render();
+      })
+
+      describe('and when user clicks on a block in the primaryBlockArray group', () => {
+        beforeEach(() => {
+          clickBlock('block_0x0');
+        })
+
+        it('should remove all adjacent blocks with the same colour', () => {
+          assertBlockColor(blockGrid, primaryBlockArray, null)
+        })
+
+        it('should not remove any blocks of a different colour', () => {
+          assertBlockColor(blockGrid, secondaryBlockArray, 'blue')
+        })
+
+        it('should drop tertiaryBlockArray group blocks into the available space', () => {
+          assertBlockColor(blockGrid, primaryBlockArray, 'green')
+        })
       });
     });
+  })
 
-    const gridB = new BlockGrid(3, 5).grid;
+  describe('when using custom height and width values', () => {
+    beforeEach(() => {
+      blockGrid = setup(3, 5);
+      blockGrid.render();
+    })
 
-    expect(gridB.length).toBe(3);
+    it('should create a multidimensional array of Blocks', () => {
+      expect(blockGrid.grid.length).toBe(3);
 
-    gridB.forEach(column => {
-      expect(column.length).toBe(5);
-    });
-  });
+      blockGrid.grid.forEach(column => {
+        expect(column.length).toBe(5);
 
-  xit('good luck, have fun!', () => {});
+        column.forEach(block => {
+          expect(block).toBeInstanceOf(Block);
+        });
+      });
+    })
+  })
 });
